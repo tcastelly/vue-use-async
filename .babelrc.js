019085017b7523acefb91dd9e316afe07b94b30e
Thomas CASTELLY <thomas@tcy.io>
2020-04-28 23:42:52 +0200

    Use modern browser for production and polyfill with unit tests

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   sourceMap: 'inline',
   presets: [
-    [require.resolve('@babel/preset-env'), { targets: { node: 'current' } }],
+    [require.resolve('@babel/preset-env'), {
+      targets:
+        isProduction ? 'last 2 versions' : { node: 'current' },
+    }],
     [require.resolve('@babel/preset-flow')],
   ],
   plugins: [
