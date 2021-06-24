668003b43f3309eb260536652c04590282cd03e1
Thomas CASTELLY <thomas@tcy.io>
2021-06-24 17:24:56 +0200

    useResult can have a map

--- src/useResult.ts
+++ src/useResult.ts
@@ -7,19 +7,23 @@ import {
 
 type NonNullable<T> = Exclude<T, null | undefined>;
 
-export default function <T, Z extends T>(res: Ref<T>, defaultRes: Z): Ref<NonNullable<Z>> {
-  const _res = ref<T>();
+export default function <T, Z extends T>(res: Ref<T>, defaultRes: NonNullable<Z>, map?: (r: NonNullable<Z>) => NonNullable<Z>): Ref<NonNullable<Z>> {
+  const _res = ref<Z>();
+
+  if (!map) {
+    map = (a) => a as NonNullable<Z>;
+  }
 
   if (defaultRes) {
-    _res.value = defaultRes;
+    _res.value = map(defaultRes);
   }
 
   watchEffect(() => {
     if (res) {
-      const unWrapRes = unref(res);
+      const unWrapRes = unref(res) as NonNullable<Z>;
 
       if (unWrapRes) {
-        _res.value = unWrapRes;
+        _res.value = map?.(unWrapRes);
       }
     }
   });
--- tests/useResult.spec.ts
+++ tests/useResult.spec.ts
@@ -2,12 +2,14 @@ import useResult from '@/useResult';
 import { ref } from 'vue';
 
 describe('GIVEN useResult', () => {
-  const resMaybeNullable = ref<undefined | null | number[]>();
+  describe('WHEN input is undefined', () => {
+    const resMaybeNullable = ref<undefined | null | number[]>();
 
-  const res = useResult(resMaybeNullable, []);
-  it('THEN res should not nullable', () => {
-    expect(Array.isArray(res.value)).toBe(true);
-    expect(res.value.length).toBeFalsy();
+    const res = useResult(resMaybeNullable, []);
+    it('THEN res should not nullable', () => {
+      expect(Array.isArray(res.value)).toBe(true);
+      expect(res.value.length).toBeFalsy();
+    });
   });
 
   describe('WHEN using inheritance', () => {
@@ -15,10 +17,24 @@ describe('GIVEN useResult', () => {
     const resA = ref<undefined | null | { id: number }>();
 
     // client part need to extend with a label
-    const resB = useResult(resA, { id: 0, label: '' });
+    const resB = useResult(resA, { id: 42, label: 'foo' });
     it('THEN extended property should not be in error', () => {
       expect(resB.value.id).toBe(42);
       expect(resB.value.label).toBe('foo');
+    });
+  });
+
+  describe('WHEN a mapper is defined', () => {
+    const _res = ref<undefined | null | number[]>();
+
+    const res = useResult(
+      _res,
+      [1],
+      (r) => r.map((item) => item * 100),
+    );
+    it('THEN is should be used', () => {
+      expect(res.value.length).toBe(1);
+      expect(res.value[0]).toBe(100);
     });
   });
 });
